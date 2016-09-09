bae964a5b9996231df8a9be85c28d02141279f0a
David Garcia <david.programming@gmail.com>
2016-09-08 21:21:01 -0500

    Inject entity manager annotation added.

---
+++ japhet-project/japhet-project-services/src/main/java/marketing/redirect/dao/annotations/InjectEntityManager.java
@@ -0,0 +1,13 @@
+package marketing.redirect.dao.annotations;
+
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.RetentionPolicy.CLASS;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+@Retention(CLASS)
+@Target(FIELD)
+public @interface InjectEntityManager {
+
+}
